fc6abfd72b6da082977c53aea3a654f1715db0ed
ninachadunelis <101791267+ninachadunelis@users.noreply.github.com>
2022-05-08 00:20:36 +0400

    some changes

--- scripts.js
+++ scripts.js
@@ -11,12 +11,15 @@ function getUsers(page) {
     requist.send();
 }
 
+//ვარენდერებ: 1. ვიღებ რისფონსივ ტექსტ მთლიანად. (this-ანუ ეგაა)
+//2. ვპარსავ ტექსტს ,ანუ გადმომაქვს ,როგორც დატა. 3. ფრაგმენტულად ვსვამ.
 
 function render() {
     let response = this.responseText;
     let responseData = JSON.parse(response);
     let fragment = document.createDocumentFragment();
-
+//ტექსტს რომ ყველას სათითაოდ გადაუაროს ფორ იჩით ვაკეთბ. და ვეუბნები რომ 
+//დინამიურად შეიქმნას ლი ელემენტი და მერე კიდევ პი . მერე სურათის და მერე ვაეპენდებ.
     responseData.data.forEach(item => {
         let li = document.createElement('li');
         li.classList.add('liInfo');
@@ -33,13 +36,13 @@ function render() {
         fragment.appendChild(li);
     
 });
-
+//
 document.getElementById('ul-list').innerHTML = ' '; 
        document.getElementById('ul-list').appendChild(fragment);
        totalPagesAp=responseData.total_pages;
 
     }
-
+//ერორის შემთხევას ვშლი აქ.
     function errorRender(){
         let p = document.createElement('p');
         p.textContent = 'server error';
@@ -54,7 +57,7 @@ document.getElementById('ul-list').innerHTML = ' ';
         getUsers(currentPage);
         
     });
-
+//კლიკის შემთხვევაში ვეუბნები რომ გადავიდეს ნექსთზე,ან უკან + = და საჭიროების შემთხვევაში იყოს ქარენთზე.
     document.getElementById('Next').addEventListener('click', function(){
         if(currentPage == totalPagesAp){
             return;
@@ -62,5 +65,5 @@ document.getElementById('ul-list').innerHTML = ' ';
         currentPage +=1;
         getUsers(currentPage);
     });
-
+//ვიძახებ მიმდინარე გვერდს.
 getUsers(currentPage);
